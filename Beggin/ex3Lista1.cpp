527ac0615e06f03f9fc23a65d76a85ff3367c11b
Paola Capita <61804889+pah-10@users.noreply.github.com>
2021-08-02 09:21:12 -0300

    Add files via upload

---
+++ Beggin/ex3Lista1.cpp
@@ -0,0 +1,17 @@
+//Exercicio 3 da lista 1
+#include <iostream>
+using namespace std;
+int main (){
+  float alt, peso, IMC;
+
+  cout<<"Informe o seu peso."<<endl;
+  cin>>peso;
+  cout<<"Informe a sua altura."<<endl;
+  cin>>alt;
+
+  IMC= peso/(alt*alt);
+
+  cout<<"O seu Índice de Massa Corporal (IMC) é: "<<IMC;
+
+return 0;
+}
